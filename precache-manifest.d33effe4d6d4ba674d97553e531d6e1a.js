59b3215711aa51766c294f425bd631e5154f6283
Martin Racana <ing.racana@gmail.com>
2020-07-24 01:23:42 +0200

    Updates

---
+++ precache-manifest.d33effe4d6d4ba674d97553e531d6e1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568aab303659a5dfb010c14eed2fc4c1",
+    "url": "/the-movie-db-api/index.html"
+  },
+  {
+    "revision": "6bcdf3df1f67f843b9f3",
+    "url": "/the-movie-db-api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3e9f0926ce6dea30b8c6",
+    "url": "/the-movie-db-api/static/css/main.86eff814.chunk.css"
+  },
+  {
+    "revision": "6bcdf3df1f67f843b9f3",
+    "url": "/the-movie-db-api/static/js/2.290d4208.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/the-movie-db-api/static/js/2.290d4208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9f0926ce6dea30b8c6",
+    "url": "/the-movie-db-api/static/js/main.5c6dd641.chunk.js"
+  },
+  {
+    "revision": "48a9b6aacb8f955b6aa5",
+    "url": "/the-movie-db-api/static/js/runtime-main.f099d7bc.js"
+  },
+  {
+    "revision": "f184bd1fef9d09a43ec7c715bfc4f58e",
+    "url": "/the-movie-db-api/static/media/no_image.f184bd1f.png"
+  }
+]);
